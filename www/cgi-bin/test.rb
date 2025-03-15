ee0cfb8df79605179146ee3b705a571c4f79a8a3
Takayuki Nakajo <tnakajo@c4b12c6.42berlin.de>
2025-03-15 19:35:04 +0100

    ruby is working ver

--- www/cgi-bin/test.rb
+++ www/cgi-bin/test.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/ruby
 require 'cgi'
+
 cgi = CGI.new
-puts "Content-Type: text/html"
-puts ""
+puts "Content-Type: text/html\r\n\r\n"
 puts "<!DOCTYPE html>"
 puts "<html><body>"
-puts "<h1>Ruby CGI</h1>"
-puts "<p>Name: #{cgi['name'].empty? ? 'N/A' : cgi['name']}</p>"
-puts "<p>Message: #{cgi['message'].empty? ? 'N/A' : cgi['message']}</p>"
+puts "<h1>Ruby CGI Debug</h1>"
+puts "<p>Script running: #{__FILE__}</p>"
+puts "<p>Environment: #{ENV.to_hash.inspect}</p>"
+puts "<p>Params: #{cgi.params.inspect}</p>"
+puts "<p>Name: #{cgi['name'].empty? ? 'N/A' : CGI.escapeHTML(cgi['name'])}</p>"
+puts "<p>Message: #{cgi['message'].empty? ? 'N/A' : CGI.escapeHTML(cgi['message'])}</p>"
 puts "</body></html>"
